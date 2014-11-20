865a9b42961a7f9c447a3a87119cd6b42a85c982
Dan Murphy <murphydbuffalo@gmail.com>
2014-11-19 20:56:36 -0500

    Set up routes/middleware with body-parser module, and created view with content and form.

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
-//Less forgiving interpreter
-'use strict';
+'use strict'; //Less forgiving interpreter
 
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
+//Parse request body as JSON 
+app.use(bodyParser.json());
+
+//Parse request URL
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.set('views', './views');
 app.set('view engine', 'ejs');
+app.set('queue', []);
 
-app.get('/', function(request, response){
-  // response.send('Testing testing testing');
-  response.render('index');
-});
+app.route('/')
+  .get(function(request, response){
+    console.log('GET /');
+    response.render('index', { queue: app.get('queue') });
+  })
 
-app.post('/', function(request, response){});
+  .post(function(request, response){
+    console.log('POST /');
+    var content = request.body.content;
+    console.log(content);
+    app.get('queue').push(content);
+    console.log(app.get('queue'));
+    response.render('index', { queue: app.get('queue') });
+  })
 
-app.put('/', function(request, response){});
-
-app.delete('/', function(request, response){});
+  .delete(function(request, response){
+    console.log('DELETE /');
+    var queue = app.get('queue');
+    response.render('index', { queue: queue.shift() });
+  });
 
 var server = app.listen(8080, function(){
   var host = server.address().address;
   var port = server.address().port;
   console.log('Listening at ' + host + ' on port ' + port + '...');
 });
-
-module.exports = app;
